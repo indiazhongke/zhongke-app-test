c4d5404b3d734bc018ce1db68ce83d7db47c8fe8
indiazhongke <indiazhongke@gmail.com>
2026-02-23 12:07:47 +0530

    Add files via upload

---
+++ models/Message.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    from: { type: String, required: true },
+    to: { type: String, required: true },
+    content: { type: String, required: true },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Message", messageSchema);
